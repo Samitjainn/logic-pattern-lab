1359a2cf42de3c0e7f85d2447c3e398a48a37cce
Samit jain <samitjain2020@gmail.com>
2026-03-08 20:00:55 -0700

    Add solid square pattern printing in Python
    Implement a program to print a solid square pattern of asterisks.

---
+++ solid_square_pattern.py
@@ -0,0 +1,11 @@
+#     Solid Square Pattern
+#          * * * * *
+#          * * * * *
+#          * * * * *
+#          * * * * *
+#          * * * * *
+
+for i in range(1,6):
+    for j in range(1,6):
+        print("*", end=" ")
+    print()
