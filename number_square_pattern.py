32bcfdd072b6d2351062b3491049a829624f5df4
Samit jain <samitjain2020@gmail.com>
2026-03-08 20:08:16 -0700

    Add number square pattern generation
    This script generates a square pattern of numbers based on a specified size.

---
+++ number_square_pattern.py
@@ -0,0 +1,15 @@
+#    Number Square Pattern
+#        1 2 3 4 5
+#        1 2 3 4 5
+#        1 2 3 4 5
+#        1 2 3 4 5
+#        1 2 3 4 5
+
+
+
+n = 5
+
+for i in range(n):
+    for j in range(1, n+1):
+        print(j, end=" ")
+    print()
