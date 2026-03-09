ccb86ccc41e7d85cf8729d8caba50b8c9faf15c7
Samit jain <samitjain2020@gmail.com>
2026-03-08 20:13:23 -0700

    Refactor row number square pattern generation
    Updated the code to generate a square pattern of row numbers.

---
+++ row_number_square_pattern.py
@@ -0,0 +1,13 @@
+#    Same Number Square Pattern
+#          1 1 1 1 1
+#          2 2 2 2 2
+#          3 3 3 3 3
+#          4 4 4 4 4
+#          5 5 5 5 5
+
+n = 5
+
+for i in range(1, n+1):
+    for j in range(n):
+        print(i, end=" ")
+    print()
